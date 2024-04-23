9339dda362f689e024a72e1a67cd97c71f25b05d
barbarahellmann <babshellmann@web.de>
2024-04-23 23:13:56 +0200

    Styling of items

---
+++ frontend/src/App.tsx
@@ -0,0 +1,47 @@
+import './App.css'
+import {Todo} from "./Todo.ts";
+import TodoCard from "./TodoCard.tsx";
+import {useEffect, useState} from "react";
+import * as axios from "axios";
+
+function App() {
+
+
+    const [todos, setTodos] = useState<Todo[]>()
+
+    useEffect(
+        () => {
+            axios.get("/api/todo")
+                .then(response => {
+                    setTodos(response.data)
+                })
+        }, []
+    )
+
+    if(!todos) {
+        return "Lade..."
+    }
+
+// TESTDATEN
+//    const todos: Todo = [
+//        {"id": "abc123",
+//            "description": "kochen",
+//            "status": "OPEN"
+//        },
+//        {"id": "abc124",
+//            "description": "putzen",
+//            "status": "OPEN"
+//        }
+//    ]
+
+  return (
+    <>
+        <h1>TODOs</h1>
+        {
+            todos.map(todo => <TodoCard todo:{todo} key{todo.id}/>)
+        }
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/TodoCard.tsx
@@ -0,0 +1,14 @@
+import {Todo} from "./Todo.ts";
+
+
+type Props = {
+    todo: Todo,
+}
+export default function TodoCard(props: Props) {
+    return (
+        <div className={"todo-card"}>
+            {props.todo.description}
+        </div>
+    );
+}
+
