d3d707937e162ad33401de2f84590593bd8efb04
barbarahellmann <babshellmann@web.de>
2024-04-24 00:08:59 +0200

    3 Columns with styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,16 @@ import {Todo} from "./Todo.ts";
 import TodoCard from "./TodoCard.tsx";
 import {useEffect, useState} from "react";
 import * as axios from "axios";
+import TodoColumn from "./TodoColumn.tsx";
+import {allPossibleTodos} from "./TodoStatus.ts";
 
+
+const columns: string[] = ["OPEN", "In_PROGRES", "DONE"]
 function App() {
 
 
     const [todos, setTodos] = useState<Todo[]>()
+
 
     useEffect(
         () => {
@@ -36,10 +41,16 @@ function App() {
 
   return (
     <>
-        <h1>TODOs</h1>
-        {
-            todos.map(todo => <TodoCard todo:{todo} key{todo.id}/>)
-        }
+        <div className={"page"}>
+            <h1>TODOs</h1>
+            {
+                allPossibleTodos.map(status => {
+                    const filteredTodos = todos.filter(todo => todo.status === status)
+                    return <TodoColumn status={status} todos={filteredTodos}/>
+
+                })
+            }
+        </div>
     </>
   )
 }
